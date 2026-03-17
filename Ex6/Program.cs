03f5ea78df72dc5f23035704b43dc776aadbf6a6
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-17 16:54:24 +0700

    nop bai 6

---
+++ Ex6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaiTap
+{
+    public class Ex6
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313");
+            Console.WriteLine("-------------------------------\n");
+
+            List<string> list = new List<string>();
+            list.Add("Thai Truong Giang");
+            list.Add("Thai Phuong Nam");
+            list.Add("Nguyen Thai Bao");
+            list.Add("Nguyen Viet Anh");
+
+            Console.WriteLine("----------------------\n");
+            Console.WriteLine("Danh sach ten: ");
+            foreach (string ten in list)
+            {
+                Console.WriteLine(ten);
+            }
+            Console.Write("\nNhap ten ban muon tim: ");
+            String Tenfind = Console.ReadLine().ToLower();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].ToLower() == Tenfind)
+                {
+                    Console.WriteLine("Ten co trong mang");
+                    break;
+                }
+                else if (i == list.Count - 1)
+                {
+                    Console.WriteLine("khong co");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
