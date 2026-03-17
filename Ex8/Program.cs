6e603363260245d84688bba0796ff881e47d871f
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-17 16:57:50 +0700

    Hoan thanh bai 8

---
+++ Ex8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace BaiTap
+{
+    public class Ex8
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313");
+            Console.WriteLine("-------------------------------\n");
+            List<int> list = new List<int>();
+            Console.WriteLine("Nhap so phan tu: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Nhap phan tu thu {i + 1}:  ");
+                int num = Convert.ToInt32(Console.ReadLine());
+                list.Add(num);
+            }
+            list.Reverse();
+            Console.WriteLine("\nDay sau khi dao nguoc");
+            for (int so = 0; so < list.Count; so++)
+            {
+                Console.Write($"{list[so]}" + " ");
+            }
+                Console.ReadKey();
+        }
+    }
+}
