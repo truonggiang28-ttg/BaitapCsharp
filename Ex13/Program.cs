99ecd4613e06c9b5bad840b5075909de1f36b5e7
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-17 17:06:00 +0700

    bai 13 ngay 17/3

---
+++ Ex13/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace BaiTap
+{
+    public class Ex13
+    {
+        public class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313");
+            Console.WriteLine("-------------------------------\n");
+            List<Student> list = new List<Student>();
+            list.Add(new Student { Id = 1, Name = "Giang" });
+            list.Add(new Student { Id = 2, Name = "Nam" });
+            list.Add(new Student { Id = 3, Name = "Bao" });
+
+            foreach (Student i in list)
+            {
+                Console.WriteLine($"Id: {i.Id} | Ho ten: {i.Name}");
+            }
+
+          
+            int dem = 0;
+            for(int j = 0; j < list.Count; j++ )
+            {
+                dem++;
+            }
+            Console.WriteLine($"\nTong so sinh vien da nhap vao la: {dem}");
+        }
+    }
+}
