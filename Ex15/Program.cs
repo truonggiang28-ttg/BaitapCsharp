957709f0cea3e483f8cd72ef3cb16adc16516620
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-17 17:09:12 +0700

    ex 15 success

---
+++ Ex15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Transactions;
+
+namespace BaiTap
+{
+    public class Ex15
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313");
+            Console.WriteLine("-------------------------------\n");
+            Dictionary<string, string> trans = new Dictionary<string, string>()
+            {
+                {"hi", "chao" },
+                {"system","he thong" },
+                {"apple","qua tao" }
+            };
+            Console.WriteLine("Nhap tu can tra: ");
+            String Tutienganh = Console.ReadLine().ToLower();
+            if (trans.ContainsKey(Tutienganh)) 
+            {
+                Console.WriteLine($"\nNghia cua tu {Tutienganh} la {trans[Tutienganh]}");
+            }
+            else
+            {
+                Console.WriteLine($"\nTu {Tutienganh} khong co trong tu dien");
+            }
+
+        }
+    }
+}
