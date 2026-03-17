9f3260357b3414229e39625e1e0479a62d5d85f6
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-17 17:04:24 +0700

    bai tap ngay 17/3/2025

---
+++ Ex12/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace BaiTap
+{
+    public class Ex12
+    {
+        public class Student
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+        }
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313");
+            Console.WriteLine("-------------------------------\n");
+            List<Student> list = new List<Student>();
+            list.Add(new Student { Id = 1, Name = "Giang" });
+            list.Add(new Student { Id = 2, Name = "Nam" });
+            list.Add(new Student { Id = 3, Name = "Bao" });
+
+            foreach (Student i in list)
+            {
+                Console.WriteLine($"Id: {i.Id} | Ho ten: {i.Name}");
+            }
+
+            Console.Write("\nNhap ten sinh vien muon tim: ");
+            String NameFind = Console.ReadLine();
+            foreach(Student j in list)
+            {
+                if (j.Name.ToLower() == NameFind.ToLower())
+                {
+                    Console.WriteLine($"Sinh vien muon tim la: Id = {j.Id} Ho ten ={j.Name}"); 
+                }
+            }
+        }
+    }
+}
