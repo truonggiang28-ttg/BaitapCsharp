41948d2cc355f0e97a55354fbf8ab8935c9bf61d
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-17 16:46:47 +0700

    Hoan thanh bai3

---
+++ Ex3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+namespace BaiTap
+{
+    public class ex3
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313");
+            Console.WriteLine("---------------------------------\n");
+            List<int> list = new List<int>();
+            list.AddRange(new int[] { 5, 7, 15, 11, 13 });
+            Console.WriteLine("Cac phan tu trong day la:");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            int Max = list[1];
+            for (int i = 0; i < list.Count; i++)
+            {
+                if(list[i] > Max)
+                    Max = list[i];
+            }
+            Console.WriteLine($"\nPhan tu lon nhat trong day la = {Max}");
+            Console.ReadKey();
+
+        }
+    }
+}
