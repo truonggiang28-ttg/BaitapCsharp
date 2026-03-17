06c5906b43b8788465d3af372d02969922f11e17
Thai Truong Giang <truonggiang121604@gmail.com>
2026-03-17 17:10:17 +0700

    bai 16 ready

---
+++ Ex16/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BaiTap
+{
+    public class Ex16
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Thai Truong Giang_2415053122313");
+            Console.WriteLine("-------------------------------\n");
+            Console.Write("Nhap mot chuoi: ");
+            string input = Console.ReadLine();
+
+            Dictionary<char, int> countDict = new Dictionary<char, int>();
+
+            foreach (char c in input)
+            {
+                if (countDict.ContainsKey(c))
+                {
+                    countDict[c]++;
+                }
+                else
+                {
+                    countDict.Add(c, 1);
+                }
+            }
+
+            Console.WriteLine("Ket qua:");
+            foreach (var item in countDict)
+            {
+                Console.Write($"{item.Key} : {item.Value}   ");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
